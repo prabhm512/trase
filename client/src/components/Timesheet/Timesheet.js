617cf946cec5db6e1aaf425114bc426d0f21b99b
Prabh Singh <prabhm512@gmail.com>
2021-01-23 21:15:53 +1100

    timesheet created and user added tasks rendering on it

--- client/src/components/Timesheet/Timesheet.js
+++ client/src/components/Timesheet/Timesheet.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import API from '../../utils/apis/API';
 
 function Timesheet() {
+
+    const renderTasks = () => {
+        API.getTasks()
+            .then(res => {
+                // Loop through initial data to find out value of last key
+                for (let key in res.data[0].tasks) {
+                    if (res.data[0].tasks.hasOwnProperty(key)) {
+
+                        const newTableRow = `
+                        <tr>
+                            <th>${res.data[0].tasks[key].content}<th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>`;
+
+                        document.querySelector(".timesheet-body").innerHTML += newTableRow;
+                    }
+                }
+            })
+    }
+    useEffect(() => {
+        renderTasks();
+    })
+
     return (
         <div className="timesheet">
             <h1>Timesheet</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Mon</th>
+                        <th scope="col">Tues</th>
+                        <th scope="col">Wed</th>
+                        <th scope="col">Thurs</th>
+                        <th scope="col">Fri</th>
+                    </tr>
+                </thead>
+                <tbody className="timesheet-body"></tbody>
+            </table>
         </div>
     )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDND from '../components/ReactDND/ReactDND';
+import { useHistory } from 'react-router-dom';
 
 function Home() {
+    const history = useHistory();
     
+    const handleClick = () => {
+        history.push('/timesheet')
+    }
     return (
-        <ReactDND></ReactDND>
+        <div className="home">
+            <ReactDND></ReactDND>
+            <button onClick={handleClick}>Timesheet</button>
+        </div>
     )
 }
 
