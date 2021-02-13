d84376e4b17ea6eb2f13a72ebd0a0ccb593494b7
Prabh Singh <prabhm512@gmail.com>
2021-02-13 13:32:48 +1100

    favicon added and build folder created in client

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ require("./routes/userRoutes")(app);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.use((req, res, next) => {
